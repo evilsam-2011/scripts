9057e1564064ed586b7b4dddd806c7dd85a851dd
Sam <sanagvesamarth07@gmail.com>
2026-06-08 01:09:07 +0530

    added tmux

--- project.sh
+++ project.sh
@@ -1,6 +1,6 @@
 SELECTED_DIR=$(zoxide query --list | vicinae dmenu --placeholder "Select option")
-
+SESSION_NAME=$(basename "$SELECTED_DIR")
 if [ -n "$SELECTED_DIR" ]; then
-    zed "$SELECTED_DIR" &
-    ghostty -e fish -c "cd \"$SELECTED_DIR\"; exec fish" &
+    zed -n "$SELECTED_DIR" &
+    ghostty -e tmux new-session -A -s "$SESSION_NAME" -c "$SELECTED_DIR" &
 fi
