c1631bd40b09632f664efe8fea5196a1dc0f5c4f
Sam <sanagvesamarth07@gmail.com>
2026-06-07 08:02:42 +0530

    Added if-statement to handle empty selection

--- project.sh
+++ project.sh
@@ -1,5 +1,6 @@
 SELECTED_DIR=$(zoxide query --list | vicinae dmenu --placeholder "Select option")
 
-zed "$SELECTED_DIR"
-
-ghostty -e fish -c "cd \"$SELECTED_DIR\"; exec fish"
+if [ -n "$SELECTED_DIR" ]; then
+    zed "$SELECTED_DIR" &
+    ghostty -e fish -c "cd \"$SELECTED_DIR\"; exec fish" &
+fi
